3f25dc7544fa23e18e3c6fc0854f01c294f07cca
ericmontelares <ericmontelares3@gmail.com>
2025-06-02 23:36:22 -0300

    feat: main menu screen

---
+++ core/src/main/java/com/raindrop/game/MainMenuScreen.java
@@ -0,0 +1,11 @@
+package com.raindrop.game;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainMenuScreen implements Screen {
+
+
+}
+
